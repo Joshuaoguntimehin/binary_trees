a2c91a72e63c269d7fcc196d761c7809155dbcd0
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-05 19:09:45 +0100

    alx tasks

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent ==NULL)
+		return (1);
+	return (0);
+}
