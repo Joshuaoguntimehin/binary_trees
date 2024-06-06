a784921e14785672a2e78fb161214325caf4271b
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-06 18:57:12 +0100

    alx tasks

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_postorder - print elements of tree using post-order traversal
  * @tree: tree to go through
@@ -7,23 +8,14 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-       	if (tree == NULL || func ==NULL)
+	if (tree == NULL || func == NULL)
 	{
 		return;
 	}
 	else
 	{
-		// Traverse the left subtree
 		binary_tree_postorder(tree->left, func);
-	
-		// Traverse the right subtree
 		binary_tree_postorder(tree->right, func);
-		
-		// Visit the root node
 		func(tree->n);
-
 	}
- 
-
-
 }
