2b0f05792fb4c2e8ed1ddb8ad0ce27195e4ea35e
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-06-06 13:09:14 +0100

    alx tasks

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,10 +5,9 @@
  * @func: function to use
  * Return: Nothing
  */
- */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-       	if (tree == NULL)
+       	if (tree == NULL || func ==NULL)
 	{
 		return;
 	}
